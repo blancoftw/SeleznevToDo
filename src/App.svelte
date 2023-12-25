561e3839ce647c748b9918d1997d83b3a33cc597
blancoftw <maxselezen@mail.ru>
2023-12-25 06:06:49 +0000

    1

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,94 @@
 <script>
-	export let name;
+	let newItem = "";
+
+	let todoList = [
+		{ text: "Write my first post", status: true },
+		{ text: "Upload the post to the blog", status: false },
+		{ text: "Publish the post at Facebook", status: false },
+	];
+
+	function addToList() {
+		todoList = [...todoList, { text: newItem, status: false }];
+		newItem = "";
+	}
+
+	function removeFromList(index) {
+		todoList.splice(index, 1);
+		todoList = todoList;
+	}
 </script>
 
-<main>
-	<h1>Hello!!!!!{name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class = "flex_X">
+
+
+</div>
+
+<div class="flex1">
+	<div class="todo">
+		
+		<h1>Список дел</h1>
+		<input bind:value={newItem} type="text" placeholder="new todo item.." />
+
+		<button on:click={addToList}> Add </button>
+		
+		
+		
+		{#each todoList as item, index}
+		<div class = "todos">
+			<div class= "item">
+
+			<input bind:checked={item.status} type="checkbox" />
+
+			<div class:checked={item.status}>
+				{item.text}
+			</div>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<div class ="del" on:click={() => removeFromList(index)}>&times;</div>
+		</div>
+	</div>				
+		{/each}
+	</div>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.item div:nth-child(2){
+width: 10em ;
+display: flex;
+flex-direction: row;
+align-items: center;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.item {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		
 	}
+	.checked {
+		text-decoration: line-through;
+	}
+	.flex1 {
+		display: flex;
+		height: 100vh;
+		justify-content: center;
+		align-items: center;
+		background: #183544;
+		padding: 4em ;
+		margin: 4em;
+	}
+	.todo {
+		display: flex;
+		flex-direction: column;
+		background: #49778f;
+		color: whitesmoke;
+		padding: 2em ;
+	}
+	.todos {
+		display: flex;
+		flex-direction: column;
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	}
+	.del {
+		font-size: 1.7em;
+		color: lawngreen;
 	}
 </style>
